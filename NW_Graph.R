939279ed334fcfe092349a2d287a7bfdc74ceb8f
inaba masahiro <ba-ina.m@ezweb.ne.jp>
2020-01-18 17:53:43 +0900

    2020/1/18

--- NW_Graph.R
+++ NW_Graph.R
@@ -201,7 +201,7 @@ ggnet2(MPnet, color="Party", palette="Set2",
 
 #地図との組み合わせ
 install.packages("maps")
-install.packages("geoshpere")
+install.packages("geosphere")
 library(network)
 library(sna)
 library(maps)
@@ -211,10 +211,222 @@ library(GGally)
 URL <- "http://snatool.g2.xrea.com/sampledata/"
 airports <- read.csv(paste(URL, "airport.csv", sep="\t"), header=T)
 rownames(airports) <- airports$airport
+class(flights)
+head(flights)
+flights <- read.csv(paste(URL, "top50.csv", sep=""), header=T)
+
+flights.net <- network(flights[, 1:2], directed=F)
+flights.net %v% "lat" <- airports[network.vertex.names(flights.net), "latitude"]
+flights.net %v% "lon" <- airports[network.vertex.names(flights.net), "longitude"]
+flights.net %v% "type" <- as.character(airports[network.vertex.names(flights.net), "type"])
+flights.net %v% "degree" <- degree(flights.net, gmode="graph")
+
+jpn <- ggplot(map_data("world2", "japan"), aes(x=long, y=lat)) +
+  geom_polygon(aes(group=group), color="grey65", fill="grey90")
+ggnetworkmap(jpn, flights.net, size=7, node.group=type,
+          node.alpha=0.6, weight=degree, ring.group=degree,
+          great.circle=T, segment.color="steelblue",
+          segment.alpha=0.4)
+
+
+
+install.packages("ggraph")
+library(ggraph)
+head(highschool)
+library(igraph)
+coleman <-  graph_from_data_frame(highschool)
+E(coleman)$year <- as.character(E(coleman)$year)
+V(coleman)$degree <- degree(coleman)
+
+ggraph(coleman) +
+  geom_edge_arc(arrow=arrow(angle=20, length=unit(2,"mm")), curvature=0.2, aes(color=year)) +
+  geom_node_point(aes(size=degree), col="gray20", alpha=0.5) +
+  theme_graph()
+
+
+install.packages("networkD3")
+library(networkD3)
+data(MisNodes)
+data(MisLinks)
+head(MisNodes)
+head(MisLinks)
+(LesMis <- forceNetwork(Links=MisLinks, Nodes=MisNodes,
+                        Source="source", Target="target", Value="value",
+                        NodeID="name", Group="group", opacity=0.8))
+saveNetwork(LesMis, file="LesMis.html", selfcontained=F)
+
+
+
+install.packages("visNetwork")
+library(visNetwork)
+library(networkD3)
+data(MisNodes)
+data(MisLinks)
+nodes <- data.frame(id=0:(nrow(MisNodes)-1), label=MisNodes$name, group=MisNodes$group)
+edges <- data.frame(from=MisLinks$source, to=MisLinks$target)
+head(nodes)
+head(edges)
+(LesMis2 <- visNetwork(nodes, edges, height="700px", width="100%"))
+visSave(LesMis2, file="LesMis2.html", selfcontained=FALSE)
+
+
+# コードダイアグラム
+install.packages("circlize")
+library(circlize)
+URL <- "http://snatool.g2.xrea.com/sampledata/"
+airports <- read.csv(paste(URL, "airport.csv", sep=""), header=T, fileEncoding="cp932")
+flights <- read.csv(paste(URL, "top50.csv", sep=""), header=T, fileEncoding="cp932")
+library(RColorBrewer)
+my.palette <- rep(c(brewer.pal(8, "Set2"), brewer.pal(9, "Set1")), 2)
+chordDiagram(flights, order=as.character(airports$airport),
+             grid.col=my.palette, transparency=0.6,
+             annotationTrack="grid", preAllocateTracks=1)
+circos.trackPlotRegion(track.index=1,
+                       panel.fun=function(x, y){
+                         xlim=get.cell.meta.data("xlim")
+                         ylim=get.cell.meta.data("ylim")
+                         sector.name=get.cell.meta.data("sector.index")
+                         circos.text(mean(xlim), ylim[1] + 0.1, sector.name,
+                                     facing="clockwise", niceFacing=T, adj=c(0, 0.5),
+                                     cex=0.7)
+                       }, bg.border=NA)
+
+
+# アークダイアグラム
+install.packages("devtools")
+library(devtools)
+install_github("gastonstat/arcdiagram")
+library(arcdiagram)
+
+URL <- "http://snatool.g2.xrea.com/sampledata/"
+airports <- read.csv(paste(URL, "airport.csv", sep=""), header=T, fileEncoding="cp932")
+flights <- read.csv(paste(URL, "top50.csv", sep=""), header=T, fileEncoding="cp932")
+edgelist <- as.matrix(flights[, 1:2])
+names <- unique(as.vector(t(edgelist)))
+col <- c("darkgoldenrod1", "cyan2")
+
+windows(width=12)
+arcplot(edgelist, lwd.arcs=flights[,3]/10^6, labels=names,
+        ordering=as.vector(airports$airport), col.label="grey30",
+        col.arcs=hsv(0, 0, 0.2, 0.25), cex.nodes=2,
+        col.nodes=col[as.integer(airports[,"type"])],
+        pch.nodes=18 - as.integer(airports[,"type"]))
+
+
+
+#ハイブプロット
+#ノードの座標に意味がある
+
+#有向グラフの隣接行列
+ADVICE <- matrix(c(
+  0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,0,1,
+  0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+  1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,0,1,1,0,1,1,
+  1,1,0,0,0,1,0,1,0,1,1,1,0,0,0,1,1,1,0,1,1,
+  1,1,0,0,0,1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+  0,1,0,0,0,1,0,0,0,0,1,1,0,1,0,0,1,1,0,0,1,
+  0,1,0,1,0,1,1,0,0,1,1,0,0,0,0,0,0,1,0,0,1,
+  1,1,0,0,0,1,1,1,0,1,1,1,0,1,0,1,1,1,0,0,1,
+  1,1,1,1,1,0,0,1,0,0,1,0,1,0,1,1,1,1,1,1,0,
+  1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+  1,1,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,
+  0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,
+  1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,
+  1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,
+  1,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+  1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,0,0,1,1,1,
+  1,1,1,0,1,0,1,0,0,1,1,0,0,1,1,0,0,1,0,1,0,
+  1,1,0,0,0,1,0,1,0,0,1,1,0,1,1,1,1,1,0,0,1,
+  0,1,1,1,0,1,1,1,0,0,0,1,0,1,0,0,1,1,0,1,0),
+  nrow = 21, byrow = TRUE)
+
+tenure <- c(9.333,19.583,12.75,7.5,3.333,
+            28,30,11.333,5.417,9.25,27,8.917,0.25,10.417,
+            8.417,4.667,12.417,9.083,4.833,11.667,12.5)
+dpt <- c(4,4,2,4,2,1,0,1,2,3,3,1,2,2,2,4,1,3,2,2,1) 
+level <- c(3,2,3,3,3,3,1,3,3,3,3,3,3,2,3,3,3,2,3,3,2)
+
+
+
+dim(ADVICE)
+length(tenure)
+length(dpt)
+length(level)
+
+install.packages("HiveR")
+library(HiveR)
+library(RColorBrewer)
+
+rownames(ADVICE) <- 1:nrow(ADVICE)
+colnames(ADVICE) <- 1:ncol(ADVICE)
+advHPD <- adj2HPD(ADVICE + t(ADVICE))
+
+advHPD$nodes$axis <- as.integer(dpt + 1)
+advHPD$nodes$radius <- tenure
+advHPD$nodes$size <- colSums(ADVICE)/10
+advHPD$nodes$color <- brewer.pal(3, "Greys")[level]
+head(advHPD$nodes)
+
+advHPD$edges$color <- brewer.pal(3, "Set2")[advHPD$edges$weight]
+head(advHPD$edges)
+
+advHPD$axis.cols <- RColorBrewer::brewer.pal(5, "Set1")
+plotHive(advHPD, ch=0, bkgnd="grey10",
+         axLabs=paste("Dpt.", 0:4), axLab.pos=5)
+
+
+plotHive(advHPD, ch=0, method="rank", bkgnd="grey10",
+         axLabs=paste("Dpt.", 0:4), axLab.pos=1,
+         rot=c(90, -18, 54, -54, 18))
 
 
 
 
+library(igraph)
+advice_igraph <- graph_from_adjacency_matrix(ADVICE)
+V(advice_igraph)$tenure <- tenure
+V(advice_igraph)$dpt <- paste("Dpt.", dpt)
+library(ggraph)
+library(purrr)
+install.packages("purrr")
+
+plot(advice_igraph)
+ggraph(advice_igraph, layout = "hive", axis = "dpt", sort.by = "tenure") +
+  geom_edge_hive(aes(alpha = ..index..), show.legend = FALSE) +
+  geom_node_point() +
+  geom_axis_hive() +
+  coord_fixed() +
+  theme_graph()
+
+
+ggraph(advice_igraph, layout="hive", axis="dpt", sort.by="tenure") +
+  geom_edge_hive(aes(alpha=..index..), show.legend=FALSE) +
+  geom_node_point() +
+  geom_axis_hive() +
+  coord_fixed() +
+  theme_graph()
+
+?ggraph
+
+library(sna)
+library(network)
+URL <- "http://snatool.g2.xrea.com/sampledata/"
+flights <- read.csv(paste(URL, "top50.csv", sep=""), header=T, fileEncoding="cp932")
+flights.net <- network(flights[, 1:2], directed = FALSE)
+pdf(file="airports.pdf", family="Japan1")
+gplot(flights.net, gmode="graph", vertex.cex=0,
+      edge.col="grey50", displaylabels=T, label.pos=5)
+dev.off()
+
+InterData <- Data1[Data1[, 1] %in% Data2[, 1],] #共通部分の抽出
+
+#パッケージ保存先のパス
+.libPaths()
+
+変更前 > URL <- "http://www.geocities.jp/snatool/sampledata/"
+変更後 > URL <- "http://snatool.g2.xrea.com/sampledata/"
 
 変更前 > install_github('arcdiagram', username='gastonstat')
 変更後 > install_github("gastonstat/arcdiagram")
